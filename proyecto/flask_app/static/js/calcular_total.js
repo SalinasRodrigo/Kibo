12d58448a6391323b874240fdef7313dfe463d1d
Tadeo <tade25.molinas@gmail.com>
2023-03-18 09:54:56 -0300

    ajustes al index y implementación del api de cambios

--- proyecto/flask_app/static/js/calcular_total.js
+++ proyecto/flask_app/static/js/calcular_total.js
@@ -4,7 +4,22 @@ document.addEventListener("DOMContentLoaded", () => {
         total += Number($(this).text());
     });
     $("#total").text(total + " Gs")
-    $("#amount").val(total/7163)
+    $("#amount").val(total/7183)
+    // var myHeaders = new Headers();
+    // myHeaders.append("apikey", "VuyHKMdNT4YKzpNMI1YJondBp989w3fU");
+    
+    // var requestOptions = {
+    //   method: 'GET',
+    //   redirect: 'follow',
+    //   headers: myHeaders
+    // };
+    
+    // fetch("https://api.apilayer.com/exchangerates_data/convert?to=USD&from=PYG&amount="+total, requestOptions)
+    //   .then(response => response.json())
+    //   .then(result => {
+    //     $("#amount").val(result["result"])
+    //   })
+    //   .catch(error => console.log('error', error));
 });
 
 
